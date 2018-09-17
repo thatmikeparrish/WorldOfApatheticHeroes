f35d37a4530ea925ab7eade5c1c26e8abfcbe229
Mike Parrish <mparrish615@gmail.com>
2018-09-16 23:29:02 -0500

    xfer to laptop

--- src/App/components/Characters/CharacterList.js
+++ src/App/components/Characters/CharacterList.js
@@ -5,11 +5,6 @@ import CharacterCard from './CharacterCard'
 
 
 export default class CharactersList extends Component {
-
-    /* activeCharacter = (el) => {
-        let characterCards = 0
-
-    } */
     
     render() {
         //console.log("Current User ID", this.props.user.id)
@@ -22,19 +17,10 @@ export default class CharactersList extends Component {
                     <div className="characters">
                     {
                         this.props.characters.map(character =>
-                            <CharacterCard key={character.id} character={character} {...this.props} changeBackground={this.props.changeBackground} delete={this.props.delete} races={this.props.races} classes={this.props.classes} />
+                            <CharacterCard key={character.id} character={character} {...this.props} delete={this.props.delete} races={this.props.races} classes={this.props.classes} />
                         )
                     }
-                    </div>
-                    {
-                        (this.props.characters.length !== 10) ?
-                            <div className="d-flex justify-content-center">
-                                <button className="btn addCharacter" onClick={this.props.addCharacter}>Add New Character</button>
-                            </div>
-
-                        :
-                            <div></div>
-                    }
+                    </div>   
                 </div>
             </React.Fragment>
         )
