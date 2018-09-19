7069340d18511b82f55e8139da8b11bf48346562
Mike Parrish <mparrish615@gmail.com>
2018-09-18 21:09:11 -0500

    card styling"

--- src/App/components/Characters/EditCard.js
+++ src/App/components/Characters/EditCard.js
@@ -25,31 +25,35 @@ export default class CharacterCard extends Component {
 
         return (
             <div key={this.props.character.id} className="editCardBackground">
-                <div className="btn editCard">
-                    <div className="characterName">
-                        {
-                            (this.state.cardEdit) ?
-                            <input type="text" className="formField" id="name" placeholder="Name" defaultValue={this.props.character.name}/>
-                            :
-                            <h5>{this.props.character.name}</h5>
-                        }
-                    </div>
-                    <div className="characterInfo">
-                        <h6>Level {this.props.character.level} {matchedRace.name} {matchedClass.name}</h6> 
-                    </div>
+                <div className="editCard">
+                    <div className="d-flex justify-content-around flex-row">
                     {
                         (this.state.cardEdit) ?
-                            <div className="btnBackground d-flex justify-content-center flex-row">
-                                <button onClick={this.toggle} className="btn characterNameBtn">Save</button>
-                                <button onClick={this.toggle} className="btn characterNameBtn">Cancel</button>
-                            </div>
+                            <input type="text" className="formField" id="name" placeholder="Name" defaultValue={this.props.character.name} />
                             :
-                            <div className="btnBackground">
-                                <button onClick={this.toggle} className="btn editNameBtn">Change Name</button>
-                            </div>
+                            <h5 className="characterName">{this.props.character.name}</h5>
                     }
                 </div>
+                <div className="characterInfo">
+                    <h6>Level {this.props.character.level} {matchedRace.name} {matchedClass.name}</h6>
+                </div>
+                {
+                    (this.state.cardEdit) ?
+                        <div className="d-flex justify-content-around flex-row">
+                            <div className="btnBackground">
+                                <button onClick={this.toggle} className="btn characterNameBtn">Save</button>
+                            </div>
+                            <div className="btnBackground">
+                                <button onClick={this.toggle} className="btn characterNameBtn">Cancel</button>
+                            </div>
+                        </div>
+                        :
+                        <div className="btnBackground">
+                            <button onClick={this.toggle} className="btn editNameBtn">Change Name</button>
+                        </div>
+                }
             </div>
+            </div >
         )
     }
 }
